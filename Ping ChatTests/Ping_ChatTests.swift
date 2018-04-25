7a6e460d2eb908d1803485b018ffe7997d61ed34
Developer <developer@faz-macbook-air.local>
2018-04-25 11:16:28 +0800

    Initial Commit

---
+++ Ping ChatTests/Ping_ChatTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ping_ChatTests.swift
+//  Ping ChatTests
+//
+//  Created by Developer on 25/04/2018.
+//  Copyright © 2018 BetaMaju. All rights reserved.
+//
+
+import XCTest
+@testable import Ping_Chat
+
+class Ping_ChatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
